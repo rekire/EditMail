c00173e596c7020392fd4202d6d6a296ba152cfa
rekire <git@reki.re>
2013-11-27 18:23:57 +0100

    documentation and license update

--- EditMail.java
+++ EditMail.java
@@ -1,7 +1,12 @@
 /**
-* Not sure about the licence yet, come back later for updates.
-* It will be something like Apache 2 (but never [L]GPL!) you must quote somewhere my name, that is all for now.
-*/
+ * @copyright
+ * This code is licensed under the Rekisoft Public License.
+ * See http://www.rekisoft.eu/licenses/rkspl.html for more informations.
+ */
+/**
+ * @package eu.rekisoft.android.controls
+ * This package contains controls provided by [rekisoft.eu](http://rekisoft.eu/). 
+ */
 package eu.rekisoft.android.controls;
 
 import java.lang.reflect.Field;
@@ -35,9 +40,11 @@ import eu.rekisoft.android.util.UiWorker;
  * EditText for email addresses.
  * 
  * @author rekire
+ * @copyright This code is licensed under the Rekisoft Public License.<br/>
+ *            See http://www.rekisoft.eu/licenses/rkspl.html for more informations.
  */
 public class EditMail extends EditText {
-  /** The delay for beginning a lookup if the email address is fine */
+	/** The delay for beginning a lookup if the email address is fine */
 	private static final int SEARCH_DELAY = 300;
 	/** The delay for showing the results after getting the lookup data. This should prevent messages while typing */
 	private static final int SHOW_DELAY = 1200;
@@ -46,14 +53,49 @@ public class EditMail extends EditText {
 	private AddressStatus status = AddressStatus.unknown;
 	private final Helper helper;
 
+	/**
+	 * Simple constructor to use when creating a EditMail from code.
+	 * 
+	 * @param context
+	 *            The Context the view is running in, through which it can access the current theme, resources, etc.
+	 */
     public EditMail(Context context) {
 		this(context, null, 0);
     }
 
+	/**
+	 * Constructor that is called when inflating a view from XML. This is called when a view is being constructed from an XML file,
+	 * supplying attributes that were specified in the XML file. This version uses a default style of 0, so the only attribute values
+	 * applied are those in the Context's Theme and the given AttributeSet.
+	 * 
+	 * <p>
+	 * The method onFinishInflate() will be called after all children have been added.
+	 * 
+	 * @param context
+	 *            The Context the view is running in, through which it can access the current theme, resources, etc.
+	 * @param attrs
+	 *            The attributes of the XML tag that is inflating the view.
+	 * @see #EditMail(Context, AttributeSet, int)
+	 */
     public EditMail(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
     }
 
+	/**
+	 * Perform inflation from XML and apply a class-specific base style. This constructor of View allows subclasses to use their own base
+	 * style when they are inflating. For example, a Button class's constructor would call this version of the super class constructor and
+	 * supply <code>R.attr.buttonStyle</code> for <var>defStyle</var>; this allows the theme's button style to modify all of the base view
+	 * attributes (in particular its background) as well as the Button class's attributes.
+	 * 
+	 * @param context
+	 *            The Context the view is running in, through which it can access the current theme, resources, etc.
+	 * @param attrs
+	 *            The attributes of the XML tag that is inflating the view.
+	 * @param defStyleAttr
+	 *            An attribute in the current theme that contains a reference to a style resource to apply to this view. If 0, no default
+	 *            style will be applied.
+	 * @see #EditMail(Context, AttributeSet)
+	 */
     public EditMail(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
@@ -88,34 +130,65 @@ public class EditMail extends EditText {
 		addTextChangedListener(helper);
     }
 	
+	/**
+	 * @return <code>true</code> if the resolver status is AddressStatus.valid.
+	 */
     public boolean isMailAddressValid() {
 		return status == AddressStatus.valid;
 	}
 
+	/**
+	 * @return the AddressStatus of the email verification.
+	 */
 	public AddressStatus getMailStatus() {
 		return status;
     }
 
+	/**
+	 * Adds a StatusChangedListener to this EditMail.
+	 * 
+	 * @param listener
+	 *            The StatusChangedListener to add.
+	 * @return always <code>true</code>.
+	 */
 	public boolean addStatusChangedListener(StatusChangedListener listener) {
 		return observers.add(listener);
 	}
     
+	/**
+	 * Removes a StatusChangedListener from this EditMail.
+	 * 
+	 * @param listener
+	 * @return <code>true</code> if the listeners were modified by this operation, <code>false</code> otherwise.
+	 */
 	public boolean removeStatusChangedListener(StatusChangedListener listener) {
 		return observers.remove(listener);
 	}
 
-	private static class Helper extends UiWorker<Void> implements TextWatcher, OnTouchListener {
+	/**
+	 * A UiWorker implementation which
+	 * 
+	 * @author René Kilczan
+	 */
+	private final static class Helper extends UiWorker<EditText> implements TextWatcher, OnTouchListener {
 		private final EditText mail;
 		private String suggestion;
 		private String currentError;
 		private final StatusChangedListener observer;
 
+		/**
+		 * Creates a new instance of EditMail.java.
+		 * 
+		 * @param field
+		 * @param listener
+		 */
 		public Helper(EditText field, StatusChangedListener listener) {
-			super(null, false, false);
+			super(field, false, false);
 			mail = field;
 			observer = listener;
 		}
 
+		/** The email validation which is executed delayed. */
 		private final Runnable doCheck = new Runnable() {
 			@Override
 			public void run() {
@@ -134,6 +207,7 @@ public class EditMail extends EditText {
 
 		@Override
 		public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			// remove all error states on text changes.
 			mail.setError(null);
 		}
 
@@ -143,10 +217,12 @@ public class EditMail extends EditText {
 
 		@Override
 		public void afterTextChanged(Editable s) {
+			// When the change is done set the internal status to pending and invoke doCheck delayed.
 			observer.statusChanged(AddressStatus.pending);
 			LazyWorker.getSharedInstance().doLater(doCheck, SEARCH_DELAY);
 		}
 
+		// this listener is invoked by the popup "Did you mean xyz@example.com?".
 		@Override
 		public boolean onTouch(View v, MotionEvent event) {
 			v.setOnTouchListener(null);
@@ -161,8 +237,9 @@ public class EditMail extends EditText {
 			return false;
 		}
 
+		// Sets the error popup on the UI thread, together with a onclick observer to the popup. Well a little hackish made but it works :-)
 		@Override
-		protected void doWork(Void data) {
+		protected void doWork(EditText mail) {
 			mail.setError(currentError);
 			try {
 				Field mErrorPopup;
@@ -189,6 +266,12 @@ public class EditMail extends EditText {
 			}
 		}
 
+		/**
+		 * The check if the email address is valid. The result is set to currentError.
+		 * 
+		 * @param address
+		 *            The email address to check.
+		 */
 		private void checkMailAddress(String address) {
 			AddressStatus result = MailChecker.validate(address);
 			Resources res = mail.getResources();
@@ -226,7 +309,18 @@ public class EditMail extends EditText {
 
     }
 
+	/**
+	 * A listener for observing email validation changes.
+	 * 
+	 * @author René Kilczan
+	 */
 	public interface StatusChangedListener {
+		/**
+		 * Invoked when the email validation status has changed.
+		 * 
+		 * @param status
+		 *            The new AddressStatus.
+		 */
 		void statusChanged(AddressStatus status);
 	}
 }
